9238d172f589a8f3fbf8cccf633c1ae1d47422e2
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2026-03-12 16:58:09 +0700

    Add Image, Animated Number

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -96,7 +96,7 @@ export function Footer() {
               </p>
 
               {/* Social Links */}
-              <div className="flex gap-4">
+              <div className="flex gap-4 items-center">
                 <motion.a
                   className="transition-colors text-muted-foreground hover:text-primary"
                   href={social.facebook}
@@ -108,7 +108,7 @@ export function Footer() {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
-                    className="size-5"
+                    className="size-6"
                     viewBox="0 0 16 16"
                   >
                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
